d459da3f27a965abd052d0d410ae7a2cded4ab34
Ben <benclarkphoto@mac.com>
2020-01-20 22:49:51 -0800

    Socke io events

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const path = require('path')
+const http = require('http')
 // Create an Express web server
 const express = require('express')
+const socketio = require('socket.io')
 // Set up a new Express server
 const app = express()
+const server = http.createServer(app)
+// Set up support for socket.io
+const io = socketio(server)
+
 //    -Listen on port 3000
 const port = process.env.PORT || 3000
 //    -Serve up the public directory
@@ -10,6 +16,17 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    // socket.emit('countUpdated', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     io.emit('countUpdated', count)
+    // })
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
